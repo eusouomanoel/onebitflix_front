abc87a3524a33f797fc1134ee1353b80feb62a77
Manoel Neto <manoeln@outlook.com>
2023-02-07 16:38:47 -0300

    Finalização de Header Genérico - Estilo - Responsividade

---
+++ pages/register.tsx
@@ -0,0 +1,18 @@
+import styles from "../styles/registerLogin.module.scss"
+import Head from "next/head"
+import HeaderGeneric from "@/src/components/common/headerGeneric";
+
+const Register = function (){
+    return(
+        <>
+            <Head>
+                <title>Onebitlix - Registro</title>
+                <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+            </Head>
+        <main>
+            <HeaderGeneric logoUrl="/" btnUrl="/login" btnContent="Quero fazer login"/>
+        </main>
+        </>
+    )
+}
+export default Register;
